1a1c8f541ea5fa3e110db1fb2a73eecd99debcc0
coderchaser <bobliao@foxmail.com>
2017-04-13 18:11:06 +0800

    初步实现查询功能，还未着色

--- tickets_querry/tickets.py
+++ tickets_querry/tickets.py
@@ -18,10 +18,53 @@ Example:
 	tickets -dg chengdu nanjing 2016-10-10
 """
 from docopt import docopt
+from prettytable import PrettyTable
 from stations import stations
 import json
 import requests
 
+class TrainsCollections(object):
+	header='checi chezhan shijian lishi yideng erdeng ruanwo yingwo yingzuo wuzuo'.split()
+
+	def __init__(self,avaliable_trains,options) :
+		self.avaliable_trains=avaliable_trains
+		self.options=options
+	def _get_duration_(self,raw_train) :
+		duration=raw_train.get('lishi').replace(':','xiaoshi')+'fen'
+		if duration.startswith('00') :
+			return duration[4:]
+		if duration.startswith('0') :
+			return duration[1:]
+		return duration
+	@property
+	def trains(self) :
+		for init_raw_train in self.avaliable_trains:
+			raw_train=init_raw_train['queryLeftNewDTO']
+			train_no=raw_train['station_train_code']
+			initial=train_no[0].lower()
+			if not self.options or initial in self.options:
+				train =[
+					train_no,
+					'\n'.join([raw_train['from_station_name'],
+						raw_train['to_station_name']]),
+					'\n'.join([raw_train['start_time'],
+						raw_train['arrive_time']]),
+					self._get_duration_(raw_train),
+					raw_train['zy_num'],
+					raw_train['ze_num'],
+					raw_train['rw_num'],
+					raw_train['yw_num'],
+					raw_train['yz_num'],
+					raw_train['wz_num']
+				]
+				yield train
+	def prettyprint(self) :
+		pt=PrettyTable()
+		pt._set_field_names(self.header)
+		for train in self.trains :
+			pt.add_row(train)
+		print(pt)
+		
 def cli():
 	"""command-line interface"""
 	arguments=docopt(__doc__)
@@ -31,11 +74,12 @@ def cli():
 	url='https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(
 		train_date,from_station,to_station
 		)
+	options=''.join([key for key,value in arguments.items() if value is True])
 	r=requests.get(url,verify=False)
-	#print(r.json())
-	with open('./a.json','w') as f:
-		a=json.dumps(r.json())
-		f.write(a)
+	avaliable_trains=r.json()['data']
+	TrainsCollections(avaliable_trains,options).prettyprint()
+	
+	
 
 if __name__ == '__main__':
 	cli()
