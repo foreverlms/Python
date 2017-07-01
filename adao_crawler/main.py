a2cf255bde66a954169cc155e73ddb7f91d3383f
coderchaser <bobliao@foxmail.com>
2017-07-01 10:55:04 +0800

    Adao_crawler about the confession thread.

---
+++ adao_crawler/main.py
@@ -0,0 +1,24 @@
+#! usr/bin/env python3
+# coding :utf-8
+
+
+"""
+@version: ??
+@author: Bob Liao
+@liscense: BSD
+@contact: codechaser1@gmail.com
+@site: https://github.com/coderchaser
+@time: 2017/6/26 22:48
+"""
+
+import output_result
+import adao_parser
+#初始串号为：https://h.nimingban.com/t/4715940?r=4715940
+url='https://h.nimingban.com/t/4715940?r=4715940'
+parser=adao_parser.Adao_Parser()
+parser.parse(url)
+all_comments=parser.all_comments
+biscuits=parser.biscuit
+# print(len(all_comments))
+# print(len(biscuits))
+output_result.write_md(biscuits,all_comments)
